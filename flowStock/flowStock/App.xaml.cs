7e0f13fcb3638cf904d70b668e6e3cb562dbb7b7
jcg122562 <51430122+jcg122562@users.noreply.github.com>
2019-12-28 15:10:26 -0500

    commit 2

--- flowStock/flowStock/App.xaml.cs
+++ flowStock/flowStock/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using flowStock.Views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace flowStock
@@ -11,7 +12,7 @@ namespace flowStock
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
